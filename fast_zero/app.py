5170f109a9e2cf011c64f18600e715b3af914dc9
Jonas Isaias <jonas.isaias.ter@estacio.br>
2024-07-11 10:52:46 -0300

    feat: add app.py

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from http import HTTPStatus
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Jesus Cristo meu Salvador'}
+
+
+@app.get('/livro', response_class=HTMLResponse)
+def read_root():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
